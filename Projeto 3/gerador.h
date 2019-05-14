3686e351e115bca2cd3f9c410d808b4a4aab45bf
Igor Souza <igormcsouza@gmail.com>
2019-05-13 21:45:33 -0300

    Fixed Bugs

--- Projeto 3/gerador.h
+++ Projeto 3/gerador.h
@@ -6,12 +6,11 @@
 #include <string>
 #include <time.h>
 
-char *v[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+char v[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
         'v', 'w', 'x', 'y', 'z'};
 
 char dicionario(int i){
-	char *padrao = v+i;
 	return v[i];
 }
 
--- Projeto 3/huffman.h
+++ Projeto 3/huffman.h
@@ -13,7 +13,10 @@ int ocorrencias(int *saida, int tam){
 int *frequencias(char *texto, int tamTexto){
     int i = 0;
     int *frequencia = new int[26];
-    while (i < 26) frequencia[i] = ocorrencias(kmp(texto, *dicionario(i), tamTexto, 1), tamTexto);
+    while (i < 26){
+        char aux[1] = {dicionario(i)};
+        frequencia[i] = ocorrencias(kmp(texto, aux, tamTexto, 1), tamTexto);
+    }
     return frequencia;    
 }
 
