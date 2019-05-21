5d5fc60b66083eef974a32e71525819f8281a1d2
Igor Souza <igormcsouza@gmail.com>
2019-05-21 18:30:37 -0300

    Constrocting a Heap

--- Projeto 3/heap.h
+++ Projeto 3/heap.h
@@ -6,18 +6,41 @@ struct No{
     int idx;
 };
 
-/* Recebe: Array Heap
-* Remove o mínimo dos elementos da Heap, ou seja, a raiz.
-* Retorna: No
-*/
-No remove_min(No *heap);
+void swap(int *i, int *j){
+	int aux;
+	aux = *i; *i = *j; *j = aux;
+}
 
 /* Recebe: Heap
 * Constroi a heap, de forma que o mínimo fica na raiz e os filhos
 * são sempre maiores que o pai.
-* Retorna: Heap
+* Retorna: nada, pois trata-se de edição em ponteiros
 */
-No *heapfy(No *heap);
+void heapfy(No *heap, int i){
+    while(true){
+        if(heap[i].weight > heap[(2*i) + 1].weight){
+            swap(heap[i], heap[(2*i) + 1]);
+            heapfy(heap, (2*i) + 1);
+        }
+        if(heap[i].weight > heap[(2*i) + 2].weight){
+            swap(heap[i], heap[(2*i) + 2]);
+            heapfy(heap, (2*i) + 2);
+        }
+        else{
+            break;
+        }
+    }
+}
 
+/* Recebe: Array Heap
+* Remove o mínimo dos elementos da Heap, ou seja, a raiz.
+* Retorna: No
+*/
+No remove_min(No *heap){
+    No *min = heap;
+    heap[0].idx = -1; heap[0].weight = -1;
+    heapfy(heap, 0);
+    return *min;
+}
 
 #endif
