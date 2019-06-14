1cd2affff304da37bb873304d49e5bec982c41ee
Igor Souza <igormcsouza@gmail.com>
2019-06-14 19:15:49 -0300

    building Dict class

--- Projeto 4/projeto4.cpp
+++ Projeto 4/projeto4.cpp
@@ -6,59 +6,64 @@
 using std::exception;
 using std::cout;
 
-template <typename T> // Me da a liberdade de usar o tipo que eu quiser
+template <typename Tchave, typename Tvalor> // Me da a liberdade de usar o tipo que eu quiser
 class ArvoreBinaria {
-   private:
-   struct Noh { T elem; Noh *esq; Noh *dir; }; // Estrutura que tem filho esq e dir
-   Noh *primeiro;
+    private:
+    struct Noh { Tchave chave; Tvalor valor ; Noh *esq; Noh *dir; }; // Estrutura que tem filho esq e dir
+    Noh *raiz;
 
-   public:
-   // Essa classe será o meu percorredor, ele vai percorrer a arvore do menor para o maior elemento.
-   class Iterador {
-		private:
-		Noh *n;
+    public:
+    /* Essa classe será o meu percorredor, ele vai percorrer a arvore do menor 
+    * para o maior elemento. */
+    class Iterador {
+        private:
 
-		public:
-		Iterador (Noh *noh) : n(noh) { }
-		void operator++ () { n = n->prox; }
-		T operator* () { return n->elem;}
-		bool operator!= (const Iterador &i) { return n != i.n; }
-   };
+        public:
 
-   // Cria uma Arvore binária vazia
-   ArvoreBinaria() : primeiro(nullptr) { }
+    };
 
-   void adicionar (T e) {
-       Noh *aux = primeiro; 
-       Noh *n = new Noh;  n->elem = e;
-       while (aux != nullptr){
-           if (aux->elem > e) aux = aux->esq;
-           else aux = aux->dir;
-       }
-       aux = n;
-   }
+    // Cria uma Arvore binária vazia
+    ArvoreBinaria() : raiz(nullptr) { }
 
-   Iterador inicio () { Iterador i(primeiro); return i; }
-   Iterador fim () { Iterador i(nullptr); return i; }
+    void adicionar (Tchave c, Tvalor v) {
+        Noh *n = new Noh;
+        n->chave = c; n->valor = v;
+
+        if(raiz != nullptr) Noh aux = *raiz;
+
+        while(true){
+            if(raiz == nullptr){ raiz = n; break; }
+            if(raiz->valor > n->valor) raiz = raiz->esq;
+            else raiz = raiz->dir;
+        }
+
+        
+        
+    }
+
+    Iterador inicio () { Iterador i(raiz); return i; }
+    Iterador fim () { Iterador i(nullptr); return i; }
 };
 
 int main () {
-   try {
-      ArvoreBinaria<double> C;
 
-      C.adicionar(1.1);
-      C.adicionar(2.2);
-      C.adicionar(3.3);
+    cout << "Starting...\n";
 
-      ArvoreBinaria<double>::Iterador fim = C.fim(); // ou "auto fim ..."
+    try {
+        ArvoreBinaria<int, double> C;
 
-      for (auto i = C.inicio(); i != fim; ++i)
-        cout << *i << '\n';
-   }
+        C.adicionar(1, 10.3);
+        C.adicionar(2, 12.3);
+        C.adicionar(3, 18.7);
 
-   catch (const exception &e) {
-       cout << "EXCEÇÃO: " << e.what() << '\n';
+        ArvoreBinaria<int, double>::Iterador fim = C.fim(); // ou "auto fim ..."
 
-       return 1;
-   }
+        //for (auto i = C.inicio(); i != fim; ++i) cout << *i << '\n';
+    }
+
+    catch (const exception &e) {
+        cout << "EXCEÇÃO: " << e.what() << '\n';
+
+        return 1;
+    }
 }
