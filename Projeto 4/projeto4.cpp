648d2a0db97c91babb4da1e335b36f7776512a30
Igor Souza <igormcsouza@gmail.com>
2019-06-16 00:20:57 -0300

    corrections at iterador and others stuff

--- Projeto 4/projeto4.cpp
+++ Projeto 4/projeto4.cpp
@@ -9,18 +9,49 @@ using std::cout;
 template <typename Tchave, typename Tvalor> // Me da a liberdade de usar o tipo que eu quiser
 class ArvoreBinaria {
     private:
-    struct Noh { Tchave chave; Tvalor valor ; Noh *esq; Noh *dir; }; // Estrutura que tem filho esq e dir
-    Noh *raiz;
+    struct Noh { Tchave chave; Tvalor valor; 
+    Noh *pai = nullptr; Noh *esq = nullptr; Noh *dir = nullptr; };
+    
+    Noh *primeiro, *raiz;
 
     public:
-    /* Essa classe será o meu percorredor, ele vai percorrer a arvore do menor 
-    * para o maior elemento. */
+    /* Classe Iterador or ++
+    * Definições importantes:
+    * a) O filho mais a esquerda é o filho a esquerda do nó k que aponta para nulo como 
+    * filho esquerdo
+    * b) O pai mais a direita é o primeiro pai do nó k, em que k é o filho esquerdo.
+    * 
+    * Algoritmo do operador++
+    * 1. Inicia no nó k
+    * 2. Se tem filho a dir, vai pra ele e procura o filho mais a esq.
+    * 3. Se não vai pro pai e procura o primeiro pai que seja mais a dir. 
+    * */
     class Iterador {
         private:
+        Noh *n;
 
         public:
+        Iterador(Noh *noh) : n(noh) {}
 
-    };
+        void operator++() {
+            Tvalor valor = n->valor;
+            while (true){
+                if (n->dir != nullptr && n->dir->valor > valor){
+                    n = n->dir;
+                    while (n->esq != nullptr) n = n->esq; break;
+                } e lse {
+                    if (n->pai != nullptr){
+                        if(n->pai->esq == n){ n = n->pai; break; }
+                        else n = n->pai;
+                    } else { n = nullptr; break; }
+                }
+            }
+        }
+
+        TV operator*() { return n->valor; }
+
+        bool operator!=(const Iterador &i) { return n != i.n; }
+};
 
     // Cria uma Arvore binária vazia
     ArvoreBinaria() : raiz(nullptr) { }
@@ -36,12 +67,14 @@ class ArvoreBinaria {
             if(raiz->valor > n->valor) raiz = raiz->esq;
             else raiz = raiz->dir;
         }
-
-        
-        
     }
 
-    Iterador inicio () { Iterador i(raiz); return i; }
+    Iterador procurar (Tchave c) {
+        Iterador i(nullptr);
+        return i; 
+    }
+
+    Iterador inicio () { Iterador i(primeiro); return i; }
     Iterador fim () { Iterador i(nullptr); return i; }
 };
 
